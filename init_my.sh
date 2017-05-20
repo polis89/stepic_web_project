9c45ce906bd5fb619b01909b2d597ff9f288d5a7
Dmitrii Polianskii <89polis@gmail.com>
2017-05-20 18:37:31 +0200

    aaaa

--- init_my.sh
+++ init_my.sh
@@ -1,2 +1,5 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+ln -s /home/polis/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+/etc/init.d/nginx restart
+ln -s /home/polis/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+/etc/init.d/gunicorn restart
+﻿/etc/init.d/mysql start﻿
